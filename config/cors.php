8b27dc63bdd59b14a92523039496b1226bef5155
redaviationsistema-ui <redaviationsistema@gmail.com>
2026-05-14 23:19:02 -0600

    M

--- config/cors.php
+++ config/cors.php
@@ -10,9 +10,12 @@ return [
         explode(',', (string) env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173'))
     ))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '^https?:\/\/localhost(:\d+)?$',
+        '^https?:\/\/127\.0\.0\.1(:\d+)?$',
+    ],
 
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
